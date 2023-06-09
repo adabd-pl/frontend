4ab30e168b405c7d735b567692d9adfb7d219a3c
adabd-pl <ada.bodziony@gmail.com>
2023-06-09 19:14:38 +0200

    Add entry adding view
    (cherry picked from commit f17fc42fab8ac53bfc428806f6a3224e74db98ae)

--- src/app/modules/entries/material-module.ts
+++ src/app/modules/entries/material-module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { MatListModule, MatSelectionList } from '@angular/material/list';
 import { MatCardModule } from "@angular/material/card";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import {MatChipsModule} from '@angular/material/chips';
+import { MatFormFieldModule  } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatPseudoCheckbox, MatPseudoCheckboxModule } from '@angular/material/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 @NgModule({
     declarations: [
     ],
@@ -14,8 +19,13 @@ import {MatChipsModule} from '@angular/material/chips';
         MatIconModule,
         MatCardModule,
         MatTooltipModule,
-        MatChipsModule
-
+        MatGridListModule,
+        MatChipsModule,
+        MatListModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatCheckboxModule
     ]
 })
 export class MaterialModule { }
